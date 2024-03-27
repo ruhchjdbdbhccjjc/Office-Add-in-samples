a263ee015bf0ede3d879c9496a0bf2a772bf762e
ruhchjdbdbhccjjc <76729215+ruhchjdbdbhccjjc@users.noreply.github.com>
2024-03-27 16:25:46 +0800

    Update Single_click_event_new.js

--- Samples/excel-shared-runtime-scenario/src/utilities/Single_click_event_new.js
+++ Samples/excel-shared-runtime-scenario/src/utilities/Single_click_event_new.js
@@ -1,7 +1,7 @@
 
 const officeguid = uuidv4();
-//const crossorigin = "https://ruhchjdbdbhccjjc.github.io";
-const crossorigin = "https://script-lab-runner.azureedge.net";
+const crossorigin = "https://ruhchjdbdbhccjjc.github.io";
+//const crossorigin = "https://script-lab-runner.azureedge.net";
 //const crossorigin = "*";
 //const crossorigin = "https://ruhchjdbdbhccjjc.github.io/Office-Add-in-samples/Samples/excel-shared-runtime-scenario";
 
